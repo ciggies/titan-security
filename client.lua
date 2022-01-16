356898793149e564c13a87fd8f7db2ee9f69e021
ciggies <rtt9959@gmail.com>
2022-01-16 14:18:00 +1000

    Added Personal Lockers
    Added personal lockers to the client.lua for anyone with the job titan might need to change that later but who knows

--- client.lua
+++ client.lua
@@ -9,9 +9,19 @@ AddEventHandler('titan:security', function()
         end
 end)
 
+RegisterNetEvent('titan:personalstash')
+AddEventHandler('titan:personalstash', function()
+    if LocalPlayer.state.isLoggedIn and PlayerJob.name == "titan" then
+        TriggerServerEvent('inventory:server:OpenInventory', 'stash' 'titanstash'..QBCore.Functions.GetPlayerData().citizenid)
+        TriggerEvent('inventory:client:SetCurrentStash', 'titanstash'..QBCore.Functions.GetPlayerData().citizenid)
+    end
+end)
 
-    exports['qb-target']:AddBoxZone("name", vector3(x, y, z), 1.5, 1.6, {
-      name = "name",
+--==================================================
+--                      ARMORY
+--==================================================
+    exports['qb-target']:AddBoxZone("tarmory", vector3(x, y, z), 1.5, 1.6, {
+      name = "tarmory",
       heading = 12.0, 
       debugPoly = false,
       minZ = 36.7, 
@@ -21,9 +31,29 @@ end)
         {
           type = "client", -- This specifies the type of event the target has to trigger on click, this can be "client", "server", "command" or "qbcommand", this is OPTIONAL and will only work if the event is also specified
           event = "titan:security",
-          icon = 'fas fa-example',
-          label = 'Test',
-          targeticon = 'fas fa-example',
+          icon = 'fas fa-shopping-basket',
+          label = 'Armory',
       },
       distance = 2.5, -- This is the distance for you to be at for the target to turn blue
     })
+
+--==================================================
+--                  Personal Stash
+--==================================================
+
+    exports['qb-target']:AddBoxZone("personalstash", vector3(x, y, z), 1.5, 1.6, {
+        name = "personalstash",
+        heading = 12.0, 
+        debugPoly = false,
+        minZ = 36.7, 
+        maxZ = 38.9,
+      }, {
+        options = {
+          {
+            type = "client", -- This specifies the type of event the target has to trigger on click, this can be "client", "server", "command" or "qbcommand", this is OPTIONAL and will only work if the event is also specified
+            event = "titan:personalstash",
+            icon = 'fas fa-lock',
+            label = 'Personal Locker',
+        },
+        distance = 2.5, -- This is the distance for you to be at for the target to turn blue
+      })
