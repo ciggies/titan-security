5fac4184a7d12fb360332cc285caaeee4f39fff8
ciggies <rtt9959@gmail.com>
2022-01-18 18:49:11 +1000

    Added qb-target config
    Added all of the qb-target stuff

--- shared.lua
+++ shared.lua
@@ -1,31 +1,31 @@
-**qb-smallreasources/server/consumables.lua**
+**qb-target/config.lua __Config.TargetModels__**
 ```lua
-QBCore.Functions.CreateUsableItems("ifak", function(source, items)
-    local src = source
-    TriggerClientEvent('consumables:client:AddHealthIfak', src)
-end)
+["titangarage"] = {
+	models = {
+		'a_m_m_afriamer_01',
+	},
+		options = {
+			{
+				type = "client",
+				event = "titan:menu",
+				icon = "fas fa-briefcase",
+				label = "ADMIN"
+			},
+		},
+		distance = 2.5,
+},
 ```
-
-**qb-smallreasources/client/consumables.lua**
+**qb-target/config.lua __Config.Peds__**
 ```lua
-    RegisterNetEvent('consumables:client:AddHealthIfak', function()
-        if GetEntityHealth(PlayerPedId()) == 100 then QBCore.Functions.Notify('You have full health', 'error') return end
-        local ped = PlayerPedId()
-        local PlayerData = QBCore.Functions.GetPlayerData()
-        QBCore.Functions.Progressbar("use_ifak", "Using IFAK..", 5000, false, true, {
-            disableMovement = false,
-            disableCarMovement = false,
-            disableMouse = false,
-            disableCombat = true,
-        }, {}, {}, {}, function() -- Done
-            if PlayerData.charinfo.gender == 0 then
-            TriggerEvent("inventory:client:ItemBox", QBCore.Shared.Items["ifak"], "remove")
-            TriggerServerEvent("QBCore:Server:RemoveItem", "ifak", 1)
-            SetEntityHealth(PlayerPedId(), GetEntityHealth(PlayerPedId()) + 20) -- the +20 is adding 20 health per use you can change to any number
-        end)
-    end)
+["titangarage"] = {
+    model = 'a_m_m_afriamer_01',
+    coords = vector4(-426.07, 1202.72, 325.76, 200.97),
+    minusOne = true,
+    freeze = true,
+    invincible = true,
+    blockevents = true,
+},
 ```
-
 
 
 
