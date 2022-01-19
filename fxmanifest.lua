cc21d03f28d9905f66ef86bc92cfd20b8d4287ab
ciggies <rtt9959@gmail.com>
2022-01-19 22:01:06 +1000

    Updated fxmanifest.lua
    Updated fxmanifest.lua to include the heli.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,10 @@ version '1.0.0'
 
 shared_script 'config.lua'
 server_script 'server.lua'
-client_script 'client.lua'
+client_script {
+	'client.lua',
+	'heli.lua'
+}
 
 --[[ui_page 'html/index.html'
 
